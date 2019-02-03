cfbaeb7db6ef2051c8709231fbc4c9e57d44e4e4
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2019-02-03 17:12:00 +0800

    Added basic app

--- app.py
+++ app.py
@@ -6,15 +6,4 @@ auth_users = []
 
 @app.route('/')
 def hello():
-    user = request.args.get('key')
-    
-    if str(user) in auth_users:
-        return f'Authorised'
-    else:
-        return 'False, make GET request to /create/<your_key>'
-
-@app.route("/create/<newkey>")
-def data(newkey):
-    newkey = request.view_args['newkey']
-    auth_users.append(newkey)
-    return f'Created, {newkey}'
+    return 'hey'
